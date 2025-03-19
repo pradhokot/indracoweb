723f0f39cd670783934414ba337c215cf4e24902
pradhokot <yoggi60@gmail.com>
2025-03-19 08:53:41 +0700

    update koneksi ucafe-php

--- ucafe-php/vardat/conn.php
+++ ucafe-php/vardat/conn.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 }
 
 // tautan
-$ucafe = "http://localhost/www/build/indracoweb/ucafe/";
+$ucafe = "http://localhost/www/build/indracoweb/ucafe-php/";
 
 
 // =======================================
